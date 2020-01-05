657186147fdded69cf10db9e1326beb64707db60
Jaeho Choe <jaeho.choe@myrealtrip.com>
2020-01-05 15:53:18 +0900

    Renamed MvSeSimpleEvent to ToSideEffect

--- mvse/src/main/kotlin/com/mrt/mvse/core/MvseSimpleEvent.kt
+++ mvse/src/main/kotlin/com/mrt/mvse/core/MvseSimpleEvent.kt
@@ -3,6 +3,6 @@ package com.mrt.mvse.core
 /**
  * Created by jaehochoe on 2020-01-05.
  */
-interface MvseSimpleEvent : MvseEvent {
-    fun <SE: MvseSideEffect> toSideEffect() : SE
+interface ToSideEffect<SE : MvseSideEffect> {
+    fun toSideEffect(): SE
 }
