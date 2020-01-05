3d031e0aec460b80dffa3076058220d789eaa7f0
Jaeho Choe <jaeho.choe@myrealtrip.com>
2020-01-05 13:20:17 +0900

    Added DoInBackground Type for MvseSideEffect

--- mvse/src/main/kotlin/com/mrt/mvse/android/MvseVm.kt
+++ mvse/src/main/kotlin/com/mrt/mvse/android/MvseVm.kt
@@ -34,17 +34,25 @@ abstract class MvseVm<S : MvseState, E : MvseEvent, SE : MvseSideEffect> : ViewM
             view(state)
             transition.sideEffect?.let { sideEffect ->
                 Mvse.log("Transition has side effect $sideEffect")
-                workThread {
-                    val toDo = bluePrint.findSideEffect(sideEffect) ?: return@workThread
-                    val result = toDo(transition).await()
-                    Mvse.log("Result is $result for $sideEffect")
-                    when (result) {
-                        is MvseState -> {
-                            mainThread { view(result as S) }
+                val toDo = bluePrint.findSideEffect(sideEffect) ?: return@model
+                when (sideEffect) {
+                    is DoInWorkThread -> {
+                        Mvse.log("Do in Background: $sideEffect")
+                        workThread {
+                            val result = toDo(transition).await()
+                            Mvse.log("Result is $result for $sideEffect")
+                            when (result) {
+                                is MvseState -> {
+                                    mainThread { view(result as S) }
+                                }
+                                is MvseEvent -> {
+                                    mainThread { intent(result as E) }
+                                }
+                            }
                         }
-                        is MvseEvent -> {
-                            mainThread { intent(result as E) }
-                        }
+                    }
+                    else -> {
+                        mainThread { toDo(transition) }
                     }
                 }
             }
---
+++ mvse/src/main/kotlin/com/mrt/mvse/core/DoInWorkThread.kt
@@ -0,0 +1,6 @@
+package com.mrt.mvse.core
+
+/**
+ * Created by jaehochoe on 2020-01-05.
+ */
+interface DoInWorkThread : MvseSideEffect
